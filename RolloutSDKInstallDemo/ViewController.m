a56b0c42d8c572c6e1ca975fb869e106e5cfc4d9
forwardto9 <forwarto9@hotmail.com>
2016-09-30 14:34:10 +0800

    Initial Commit

---
+++ RolloutSDKInstallDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RolloutSDKInstallDemo
+//
+//  Created by uwei on 9/30/16.
+//  Copyright © 2016 Tencent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
